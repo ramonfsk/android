a3e10e5b3bc8c37c2c34d021919cc4378fe9e8d3
Ramon Ferreira <ramonfsk@gmail.com>
2020-03-17 11:10:50 -0300

    aula 4.3 - Utilizacao do icone inserido e entendimento da classe AppCompatActivity

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -6,15 +6,17 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Lista de Alunos");
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(Arrays.asList(
                 "Alex",
