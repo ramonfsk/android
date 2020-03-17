a7291d999f66b2d2ce50763497eb7e5af17b3cf9
Ramon Ferreira <ramonfsk@gmail.com>
2020-03-17 11:52:03 -0300

    aula 4.6 - Criando uma nova Activity e refatorando a Main usando Shift+F6

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,6 +1,5 @@
-package br.com.alura.agenda;
+package br.com.alura.agenda.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -12,12 +11,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.alura.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Lista de Alunos");
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         List<String> alunos = new ArrayList<>(Arrays.asList(
                 "Alex",
                 "Fran",
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 "Maria",
                 "Ana"
         ));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_de_alunos);
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
