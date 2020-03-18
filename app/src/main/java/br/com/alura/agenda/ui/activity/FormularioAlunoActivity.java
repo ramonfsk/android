b7937d328be6013fb9601c546f3c763ce4df1987
Ramon Ferreira <ramonfsk@gmail.com>
2020-03-18 11:05:13 -0300

    aula 5.8 - Refatoracao no codigo e utilizacao do metodo de extracao no codigo para deixar mais claro e coeso

--- app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,13 +1,11 @@
 package br.com.alura.agenda.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.alura.agenda.R;
 import br.com.alura.agenda.dao.AlunoDAO;
@@ -15,31 +13,49 @@ import br.com.alura.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Novo aluno";
+    private EditText txtNome;
+    private EditText txtTelefone;
+    private EditText txtEmail;
+    final AlunoDAO dao = new AlunoDAO();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
-        setTitle("Novo Aluno");
+        setTitle(TITULO_APPBAR);
 
-        final AlunoDAO dao = new AlunoDAO();
+        inicializacaoDosCampos();
+        configuraBotaoSalvar();
+    }
 
-        final EditText txtNome = findViewById(R.id.activity_formulario_aluno_nome);
-        final EditText txtTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
-        final EditText txtEmail = findViewById(R.id.activity_formulario_aluno_email);
-
+    private void configuraBotaoSalvar() {
         Button btnSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nome = txtNome.getText().toString();
-                String telefone = txtTelefone.getText().toString();
-                String email = txtEmail.getText().toString();
-
-                Aluno alunoCriado = new Aluno(nome, telefone, email);
-                dao.salva(alunoCriado);
-
-                finish();
+                Aluno alunoCriado = criaAluno();
+                salva(alunoCriado);
             }
         });
+    }
+
+    private void inicializacaoDosCampos() {
+        txtNome = findViewById(R.id.activity_formulario_aluno_nome);
+        txtTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        txtEmail = findViewById(R.id.activity_formulario_aluno_email);
+    }
+
+    private void salva(Aluno aluno) {
+        dao.salva(aluno);
+        finish();
+    }
+
+    private Aluno criaAluno() {
+        String nome = txtNome.getText().toString();
+        String telefone = txtTelefone.getText().toString();
+        String email = txtEmail.getText().toString();
+
+        return new Aluno(nome, telefone, email);
     }
 }
